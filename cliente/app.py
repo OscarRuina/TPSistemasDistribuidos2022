56e8fa719029f77b068706a9a190acec605e8ced
Gonzalo Cerbelli <Gonza.cerbelli@gmail.com>
2022-08-29 13:51:01 -0300

    Cliente (#5)
    * Create .gitignore
    
    Agregando git ignore para cada uno tener su configuracion de java o de sql.
    
    * add getUser and registerUser methods to client
    
    * update readme client methods
    
    Co-authored-by: QuimeyPerez <perezquimey@gmail.com>

--- cliente/app.py
+++ cliente/app.py
@@ -1,21 +1,59 @@
-from flask import Flask
+from flask import Flask, request
 
 import logging
 
 import grpc
-import helloworld_pb2
-import helloworld_pb2_grpc
+import user_pb2
+import user_pb2_grpc
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def home():
     return "Hello, Flask!"
 
-@app.route("/helloWorldCliente", methods=["GET"])
-def grpcMessage():
+
+@app.route("/user", methods=["POST"])
+def registerUser():
+    name = request.form['name']
+    lastname = request.form['lastname']
+    email = request.form['email']
+    username = request.form['username']
+    password = request.form['password']
+
     with grpc.insecure_channel('localhost:50051') as channel:
-        stub = helloworld_pb2_grpc.GreeterStub(channel)
-        response = stub.SayHello(helloworld_pb2.HelloRequest(name='you'))
-    print("Greeter client received: " + response.message)
-    return "Greeter client received: " + response.message
+        stub = user_pb2_grpc.userServicer(channel)
+        registeredUser = stub.register(user_pb2.RegisterRequest(
+            name=name, lastname=lastname, email=email, username=username, password=password))
+
+    registeredUserResponse = {
+        "id": registeredUser["id"],
+        "name": registeredUser["name"],
+        "lastname": registeredUser["lastname"],
+        "email": registeredUser["lastname"],
+        "username": registeredUser["username"]
+    }
+
+    print("Registered User " + registeredUserResponse)
+    return registeredUserResponse
+
+
+@app.route("/user", methods=["GET"])
+def getUser():
+    userId = request.args.get('id')
+
+    with grpc.insecure_channel('localhost:50051') as channel:
+        stub = user_pb2_grpc.userServicer(channel)
+        user = stub.get(user_pb2.GetById(id=userId))
+
+        userResponse = {
+            "id": user["id"],
+            "name": user["name"],
+            "lastname": user["lastname"],
+            "email": user["lastname"],
+            "username": user["username"]
+        }
+
+    print("User " + userId + " found")
+    return userResponse
