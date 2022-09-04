7457688a2ebb89b1162b3776cae18b7511d9f04e
Gonzalo Cerbelli <gonza.cerbelli@gmail.com>
2022-09-04 17:05:27 -0300

    add product proto builds and write create and getProduct methods

--- cliente/app.py
+++ cliente/app.py
@@ -1,12 +1,15 @@
 from flask import Flask, request
 
 import logging
+import json
 
 import grpc
 import user_pb2
 import user_pb2_grpc
 import wallet_pb2
 import wallet_pb2_grpc
+import product_pb2
+import product_pb2_grpc
 
 app = Flask(__name__)
 
@@ -37,7 +40,7 @@ def registerUser():
 @app.route("/user", methods=["GET"])
 def getUser():
     userId = int(request.args.get('id'))
-    #userId = int(request.json['id'])
+    # userId = int(request.json['id'])
 
     with grpc.insecure_channel('localhost:9090') as channel:
         stub = user_pb2_grpc.userStub(channel)
@@ -124,6 +127,86 @@ def subtractwallet():
         }
 
     return json
+
+
+@app.route('/product', methods=['POST'])
+def createProduct():
+    name = request.json["name"]
+    category = request.json["category"]
+    quantity = int(request.json["quantity"])
+    price = float(request.json["price"])
+    date = request.json["date"]
+    userId = int(request.json["userId"])
+    photos = request.json["photos"]
+
+    with grpc.insecure_channel('localhost:9090') as channel:
+        stub = product_pb2_grpc.productStub(channel)
+        product = stub.create(product_pb2.RequestProduct(name=name, category=category,
+                              quantity=quantity, price=price, date=date, userId=userId, photos=photos))
+        print(product)
+
+        PHOTOS = []
+
+        for photo in product.__getattribute__("photos"):
+            photosJson = {
+                "url": photo.__getattribute__("url"),
+                "order": photo.__getattribute__("order")
+            }
+            PHOTOS.append(photosJson)
+
+        productResponse = {
+            "name": product.__getattribute__("name"),
+            "category": product.__getattribute__("category"),
+            "quantity": product.__getattribute__("quantity"),
+            "price": product.__getattribute__("price"),
+            "date": product.__getattribute__("date"),
+            "userId": product.__getattribute__("userId"),
+            "photos": PHOTOS
+        }
+
+    return productResponse
+
+
+@app.route('/product', methods=['GET'])
+def getProduct():
+    userId = int(request.args.get('userId'))
+
+    with grpc.insecure_channel('localhost:9090') as channel:
+        stub = product_pb2_grpc.productStub(channel)
+        productList = stub.getProductByUserId(
+            product_pb2.RequestProductByUserId(userId=userId))
+        print(productList)
+
+        PRODUCTS = []
+
+        for product in productList:
+
+            PHOTOS = []
+
+            for photo in product.__getattribute__("photos"):
+                photosJson = {
+                    "url": photo.__getattribute__("url"),
+                    "order": photo.__getattribute__("order")
+                }
+                PHOTOS.append(photosJson)
+
+            productJson = {
+                "name": product.__getattribute__["name"],
+                "category": product.__getattribute__["category"],
+                "quantity": product.__getattribute__["quantity"],
+                "price": product.__getattribute__["price"],
+                "date": product.__getattribute__["date"],
+                "userId": product.__getattribute__["userId"],
+                "photos": PHOTOS
+            }
+
+            PRODUCTS.append(productJson)
+
+    productResponse = {
+        "products": PRODUCTS
+    }
+
+    return productResponse
 
 
 if __name__ == '__main__':
