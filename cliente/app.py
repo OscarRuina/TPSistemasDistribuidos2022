181ed9284e7fb48e94491c42f8e95d8bdd4e4aac
Gonzalo Cerbelli <Gonza.cerbelli@gmail.com>
2022-09-03 22:11:37 -0300

    Cliente (#19)
    * Create .gitignore
    
    Agregando git ignore para cada uno tener su configuracion de java o de sql.
    
    * add getUser and registerUser methods to client
    
    * update readme client methods
    
    * qperez
    
    Compilado los protos a la carpeta python, ya se puede diseñar las funciones.
    
    * rebuild proto user client
    
    * refactor user methods, add login and logout and rebuild user proto grpc files
    
    * remove readme client methods
    
    * formatter app.py file
    
    Co-authored-by: QuimeyPerez <perezquimey@gmail.com>

--- cliente/app.py
+++ cliente/app.py
@@ -25,57 +25,66 @@ def registerUser():
     password = request.form['password']
 
     with grpc.insecure_channel('localhost:50051') as channel:
-        stub = user_pb2_grpc.userServicer(channel)
-        registeredUser = stub.register(user_pb2.RegisterRequest(
+        stub = user_pb2_grpc.userStub(channel)
+        response = stub.register(user_pb2.RegisterRequest(
             name=name, lastname=lastname, email=email, username=username, password=password))
 
-    registeredUserResponse = {
-        "id": registeredUser["id"],
-        "name": registeredUser["name"],
-        "lastname": registeredUser["lastname"],
-        "email": registeredUser["lastname"],
-        "username": registeredUser["username"]
-    }
-
-    print("Registered User " + registeredUserResponse)
-    return registeredUserResponse
+    print("Registered User " + response)
+    return response
 
 
 @app.route("/user", methods=["GET"])
 def getUser():
     userId = request.args.get('id')
 
-    with grpc.insecure_channel('localhost:50051') as channel:
-        stub = user_pb2_grpc.userServicer(channel)
-        user = stub.get(user_pb2.GetById(id=userId))
+    with grpc.insecure_channel('localhost:9090') as channel:
+        stub = user_pb2_grpc.userStub(channel)
+        response = stub.get(user_pb2.GetById(id=userId))
+        print(response)
 
-        userResponse = {
-            "id": user["id"],
-            "name": user["name"],
-            "lastname": user["lastname"],
-            "email": user["lastname"],
-            "username": user["username"]
-        }
+    return response.__str__()
 
-    print("User " + userId + " found")
-    return userResponse
+
+@app.route("/login", methods=["POST"])
+def login():
+    username = request.form['username']
+    password = request.form['password']
+
+    with grpc.insecure_channel('localhost:9090') as channel:
+        stub = user_pb2_grpc.userStub(channel)
+        response = stub.login(user_pb2.LoginRequest(
+            username=username, password=password))
+        print(response)
+
+    return response.__str__()
+
+
+@app.route("/logout", methods=["POST"])
+def login():
+    with grpc.insecure_channel('localhost:9090') as channel:
+        stub = user_pb2_grpc.userStub(channel)
+        response = stub.login(user_pb2.LogoutResponse())
+        print(response)
+
+    return response.__str__()
 
 
 @app.route("/addWallet", methods=["POST"])
 def addwallet():
     balance = int(request.json['balance'])
     userid = int(request.json['userId'])
-    #registerRequestWallet = wallet_pb2_grpc.wallet Para ver las opciones que tiene.
+    # registerRequestWallet = wallet_pb2_grpc.wallet Para ver las opciones que tiene.
 
     with grpc.insecure_channel('localhost:9090') as channel:
         stub = wallet_pb2_grpc.walletStub(channel)
-        response = stub.add(wallet_pb2.RegisterRequestWallet(balance=balance, userId=userid))
+        response = stub.add(wallet_pb2.RegisterRequestWallet(
+            balance=balance, userId=userid))
 
         #response = json.dumps(response.__str__())
         #response = response.__str__()
         #response = response.replace("\n", ",")
         #iid = response[0:5]
-        #print(iid)
+        # print(iid)
         print(response)
 
     return response.__str__()
@@ -88,11 +97,13 @@ def subtractwallet():
 
     with grpc.insecure_channel('localhost:9090') as channel:
         stub = wallet_pb2_grpc.walletStub(channel)
-        response = stub.subtract(wallet_pb2.RegisterRequestWallet(balance=balance, userId=userid))
+        response = stub.subtract(wallet_pb2.RegisterRequestWallet(
+            balance=balance, userId=userid))
         print(response)
 
     return response.__str__()
 
+
 if __name__ == '__main__':
-	logging.basicConfig()
-	app.run()
+    logging.basicConfig()
+    app.run()
