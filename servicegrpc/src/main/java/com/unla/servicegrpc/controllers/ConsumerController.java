80aa8ff5940fd909c056ec6ffd222a767e7749b7
Oscar <oscarruina@hotmail.com>
2022-09-21 18:10:32 -0300

    Added json parse

--- servicegrpc/src/main/java/com/unla/servicegrpc/controllers/ConsumerController.java
+++ servicegrpc/src/main/java/com/unla/servicegrpc/controllers/ConsumerController.java
@@ -1,7 +1,6 @@
 package com.unla.servicegrpc.controllers;
 
-import java.util.Arrays;
-import java.util.List;
+import org.json.JSONObject;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,6 +18,11 @@ public class ConsumerController {
         String url = "http://127.0.0.1:5000/messages?groupId=" + groupId + "&topic=" + topic;
         RestTemplate restTemplate = new RestTemplate();
         String messages = restTemplate.getForObject(url,String.class);
+        JSONObject json = new JSONObject(messages);
+        String topic1 = json.getString("topic");
+        /** Topics Name: products - auction - invoice **/
+        /** auction: save **/
+        /** invoice: save **/
         return messages;
     }
 
